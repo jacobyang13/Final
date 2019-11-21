d47ec5fabd48fc077c65190fba08c29f15d5b000
noahmerenbloom <noahgm@live.unc.edu>
2019-11-21 16:02:37 -0500

    commit

--- example.js
+++ example.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-I fucking jvg eat ass
-=======
-I fucking eat ass
-bigg ass tatas 
->>>>>>> eaf3adc32ae70ee9f12262bea830f7d324c86f0f
+I; fucking; eat; ass;
+bigg; ass; tatas; 
+and; a; nice; weener;
