3dcc36c7202610dbb8824da3b28edc36cdef4f14
nmerenbloom <57159120+nmerenbloom@users.noreply.github.com>
2019-12-09 15:50:49 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,112 @@
+/**
+ * Course: COMP 426
+ * Assignment: a05
+ *
+ * This file defines data to be rendered to the page.
+ */
+
+
+/**
+ * Represents a list of heroic users
+ */
+const heroicData = [{
+    id: 0,
+    name: "Hibachi & Co",
+    adress: "155 E Franklin St, Chapel Hill, NC 27514",
+    hours: "11AM-10PM",
+    img: "r_icons/hibachi.png",
+    color: "black",
+    backgroundColor: "white",
+    kosher_options: false,
+    gf: true,
+    nut_friendly: true,
+}, {
+    id: 1,
+    name: "Moe's Southwest Grill",
+    adress: "155 E Franklin St, Chapel Hill, NC 27514",
+    hours: "11AM-10PM",
+    img: "r_icons/moes.png",
+    color: "black",
+    backgroundColor: "#DED9D7",
+    kosher_options: false,
+    gf: true,
+    nut_friendly: true,
+}, {
+    id: 2,
+    name: "Chipoltle Mexican Grill",
+    adress: "155 E Franklin St, Chapel Hill, NC 27514",
+    hours: "11AM-10PM",
+    img: "r_icons/chipoltle.png",
+    color: "black",
+    backgroundColor: "white",
+    kosher_options: true,
+    gf: true,
+    nut_friendly: true,
+}, {
+    id: 3,
+    name: "The Pizza Press",
+    adress: "155 E Franklin St, Chapel Hill, NC 27514",
+    hours: "11AM-10PM",
+    img: "r_icons/pizzapress.png",
+    color: "black",
+    backgroundColor: "#DED9D7",
+    kosher_options: false,
+    gf: false,
+    nut_friendly: true,
+}, {
+    id: 4,
+    name: "Bandito's Mexican Cafe",
+    adress: "155 E Franklin St, Chapel Hill, NC 27514",
+    hours: "11AM-10PM",
+    img: "r_icons/banditos.jpg",
+    color: "black",
+    backgroundColor: "white",
+    kosher_options: false,
+    gf: true,
+    nut_friendly: true,
+}, {
+    id: 5,
+    name: "Ms. Mong",
+    adress: "155 E Franklin St, Chapel Hill, NC 27514",
+    hours: "11AM-10PM",
+    img: "r_icons/mong.png",
+    color: "black",
+    backgroundColor: "#DED9D7",
+    kosher_options: false,
+    gf: false,
+    nut_friendly: false,
+}, {
+    id: 6,
+    name: "Sutton's Drug Store",
+    adress: "155 E Franklin St, Chapel Hill, NC 27514",
+    hours: "11AM-10PM",
+    img: "r_icons/suttons.jpg",
+    color: "black",
+    backgroundColor: "white",
+    kosher_options: false,
+    gf: false,
+    nut_friendly: true,
+}, {
+    id: 7,
+    name: "Panera Bread",
+    adress: "155 E Franklin St, Chapel Hill, NC 27514",
+    hours: "11AM-10PM",
+    img: "r_icons/panera.png",
+    color: "black",
+    backgroundColor: "#DED9D7",
+    kosher_options: true,
+    gf: false,
+    nut_friendly: true,
+}, {
+    id: 8,
+    name: "Chabad House",
+    adress: "155 E Franklin St, Chapel Hill, NC 27514",
+    hours: "11AM-10PM",
+    img: "r_icons/chabad.jpg",
+    color: "black",
+    backgroundColor: "white",
+    kosher_options: true,
+    gf: false,
+    nut_friendly: false,
+}
+];
