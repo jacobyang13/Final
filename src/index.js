f3f766feb61f1316527f53fdb05983776bbce1d2
noahmerenbloom <noahgm@live.unc.edu>
2019-12-11 15:46:25 -0500

    nothing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import MainApp from './MainApp';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 ReactDOM.render(<MainApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
