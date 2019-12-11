ebe64c8e649e6dccd6cc6d944db9cdd566f6c65b
noahmerenbloom <noahgm@live.unc.edu>
2019-12-11 13:54:31 -0500

    login/register

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -15,7 +15,7 @@ function Nav() {
         <a id= "home" className="nav-link" tabIndex="1" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Features</a>
+        <a className="nav-link" href="#">Card Page</a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="#">Pricing</a>
